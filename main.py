aa29182eda60b9521de3daa96e9558ae0943a76a
Eric <oericcorreia@gmail.com>
2025-07-23 22:44:42 -0300

    nova ideia visual criada para futuras atualizações

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from tkinter import ttk
 from tkinter import messagebox
 import datetime
 import os
+from PIL import Image, ImageTk
+import pyperclip
 import random
 import string
 
 
 root = tk.Tk()
 root.title("Gerador de senhas")
-root.geometry("300x480")# dimensões do programa
+root.geometry("500x400")# dimensões do programa
 
 # cria uma pasta que vai armazenar arquivos txt. com as senhas geradas
 pasta_senhas = os.path.join("senhas geradas")
@@ -23,19 +25,44 @@ except Exception as a:
 
 
 # layout do programa utilizando o tk.Canvas
-layout = tk.Canvas(root, width=300, 
+layout = tk.Canvas(root, width=500, 
                 height=480, 
-                bg="#00FFFF")
+                bg="#ebf5f7")
 layout.pack()
 
 # frame que vai servir como container para adcionar os widgets ao layout e ter um design melhor
-frame = tk.Frame(layout, bg="#00FFFF")
-frame.place(relx=0.5, rely=0.5, anchor="center") # centraliza o frame no canvas
+frame1 = tk.Frame(root, borderwidth=1, 
+                    relief="flat", bg="#ebf5f7",)
+#relief (flat, raised, sunken, solid)
+frame1.place(x=10, y=25, 
+            width=480, 
+            height=67 ) # move o frame no canvas
 
-layout.create_line(10, 50, 290, 50) # topo horizontal
-layout.create_line(10, 50, 10, 440) # esquerdo vertical
-layout.create_line(290, 50, 290, 440) # direito vertical
-layout.create_line(290, 440, 10, 440) # baixo horizontal
+# onde estão todos os botões
+frame2 = tk.Frame(root, borderwidth=1, 
+                    relief="flat", bg="#ebf5f7")
+#relief (flat, raised, sunken, solid)
+frame2.place(x=10, y=180, 
+            width=480, 
+            height=210 )
+
+#frame onde está o resultado da senha que é gerada
+frame3 = tk.Frame(root, borderwidth=1, 
+                    relief="sunken", 
+                    background="#7aebff",
+                    highlightbackground="#c8cbcc", 
+                    highlightcolor="#c8cbcc", 
+                    highlightthickness=1)
+#relief (flat, raised, sunken, solid)
+frame3.place(x=10, y=100, 
+            width=480, 
+            height=50 )
+
+
+#layout.create_line(10, 50, 290, 50) # topo horizontal
+#layout.create_line(10, 50, 10, 440) # esquerdo vertical
+#layout.create_line(290, 50, 290, 440) # direito vertical
+#layout.create_line(290, 440, 10, 440) # baixo horizontal
 
 
 
@@ -44,11 +71,19 @@ style = ttk.Style()
 style.theme_use('clam')
 
 # label que cria um texto
-titulo = tk.Label(frame, text="Gerador de Senhas", 
-                  fg="red", 
-                  font=("Georgia", 18, "bold"), 
-                  bg="lightgreen")
-titulo.pack(pady=45)
+titulo1 = tk.Label(frame1, text="Gerador de Senhas", 
+                  fg="black",
+                  bg="#ebf5f7", 
+                  font=("Arial", 18, "bold"), 
+                  )
+titulo1.place(x=125, y=9)
+#subtitulo
+titulo2 = tk.Label(frame1, text="Crie sua senha simples e forte em um clique!", 
+                  fg="black",
+                  bg="#ebf5f7", 
+                  font=("Arial", 10), 
+                  )
+titulo2.place(x=110, y=40)
 
 
 # função principal que contem o código capaz de gerar a senha de forma aleatória
@@ -89,6 +124,8 @@ def arquivo_senha():
     except Exception as e:
         print(f"Erro ao salvar arquivo: {e}")
 
+    
+
      
 def fechar_app():
     root.quit()
@@ -113,31 +150,31 @@ style.map("TButton",
          )
 
 # botão que chama a função que faz imprimir a senha gerada no layout
-botão_gerar = ttk.Button(frame, text="Gerar senha", 
+botão_gerar = ttk.Button(frame2, text="Gerar senha", 
                          command=atualiza_senha, 
                          style="TButton")
-botão_gerar.pack(pady=20)
+botão_gerar.place(x=20, y=15)
 
 #botão que faz salvar a senha gerada
-botão_salvar = ttk.Button(frame, text="Salvar senha",
+botão_salvar = ttk.Button(frame2, text="Salvar senha",
                             command=arquivo_senha,
                             style="TButton")
-botão_salvar.pack(after=botão_gerar)
+botão_salvar.place(x=178, y=15)
 
 # faz fechar programa
-botão_sair = ttk.Button(frame, text="Sair",
+botão_sair = ttk.Button(frame2, text="Sair",
                             command=fechar_app,
                             style="botao_sair.TButton")
-botão_sair.pack(pady=20, after=botão_salvar)
+botão_sair.place(x=335, y=15)
 
 
 # label com a configuração do texto da senha gerada
-label_resultado = tk.Label(frame, text="",
-                           font=("Courier", 14, "bold"), 
+label_resultado = tk.Label(frame3, 
+                           font=("Arial", 14, "bold"), 
                            fg="#00008B", 
-                           bg="lightgreen", 
+                           bg="#7aebff",
                            wraplength=280)
-label_resultado.pack(pady=5, before=botão_gerar)
+label_resultado.place(rely=0.5, relx=0.5, anchor="center")
 
 
 
